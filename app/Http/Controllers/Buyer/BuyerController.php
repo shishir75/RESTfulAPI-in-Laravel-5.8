6d69c6c03186f1279a00d2ba9c7a20acb8b6f48b
shishir75 <shishir.srdr16@gmail.com>
2019-09-30 03:02:00 +0600

    Improving the current RESTful API operations

--- app/Http/Controllers/Buyer/BuyerController.php
+++ app/Http/Controllers/Buyer/BuyerController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\Buyer;
 
 use App\Buyer;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 
-class BuyerController extends Controller
+class BuyerController extends ApiController
 {
     /**
      * Display a listing of the resource.
@@ -17,7 +17,7 @@ class BuyerController extends Controller
     {
         $buyers = Buyer::has('transactions')->get();
 
-        return response()->json(['data' => $buyers], 200);
+        return $this->showAll($buyers);
     }
 
 
@@ -31,6 +31,6 @@ class BuyerController extends Controller
     {
         $buyer = Buyer::has('transactions')->findOrFail($id);
 
-        return response()->json(['data' => $buyer], 200);
+        return $this->showOne($buyer);
     }
 }
--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -4,9 +4,9 @@ namespace App\Http\Controllers\User;
 
 use App\User;
 use Illuminate\Http\Request;
-use App\Http\Controllers\Controller;
+use App\Http\Controllers\ApiController;
 
-class UserController extends Controller
+class UserController extends ApiController
 {
     /**
      * Display a listing of the resource.
@@ -17,7 +17,7 @@ class UserController extends Controller
     {
         $users = User::all();
 
-        return  response()->json(['data' => $users], 200);
+        return $this->showAll($users);
     }
 
     /**
@@ -44,7 +44,7 @@ class UserController extends Controller
 
         $user = User::create($data);
 
-        return  response()->json(['data' => $user], 201);
+        return $this->showOne($user, 201);
     }
 
     /**
@@ -56,7 +56,7 @@ class UserController extends Controller
     public function show($id)
     {
         $user = User::findOrFail($id);
-        return response()->json(['data' => $user], 200);
+        return $this->showOne($user);
     }
     /**
      * Update the specified resource in storage.
@@ -96,19 +96,19 @@ class UserController extends Controller
         {
             if (!$user->isVerified())
             {
-                return response()->json(['error' => 'Only verified users can modify the admin filed!', 'code' => 409], 409);
+                return $this->errorResponse('Only verified users can modify the admin filed!', 409);
             }
             $user->admin = $request->input('admin');
         }
 
         if (!$user->isDirty())
         {
-            return response()->json(['error' => 'You need to specify a different value to update', 'code' => 422], 422);
+            return $this->errorResponse('You need to specify a different value to update', 422);
         }
 
         $user->save();
 
-        return response()->json(['data' => $user], 200);
+        return $this->showOne($user);
 
     }
 
@@ -124,6 +124,6 @@ class UserController extends Controller
 
         $user->delete();
 
-        return response()->json(['data' => $user], 200);
+        return $this->showOne($user);
     }
 }

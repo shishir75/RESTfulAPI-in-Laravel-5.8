34ba97b9185c602c9949ebbcb34117b74c4f871c
shishir75 <shishir.srdr16@gmail.com>
2019-09-30 23:54:51 +0600

    Handaling Error & Exceptions

--- app/Exceptions/Handler.php
+++ app/Exceptions/Handler.php
@@ -4,9 +4,15 @@ namespace App\Exceptions;
 
 use App\Traits\ApiResponser;
 use Exception;
+use Illuminate\Auth\Access\AuthorizationException;
+use Illuminate\Auth\AuthenticationException;
 use Illuminate\Database\Eloquent\ModelNotFoundException;
+use Illuminate\Database\QueryException;
 use Illuminate\Foundation\Exceptions\Handler as ExceptionHandler;
 use Illuminate\Validation\ValidationException;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Symfony\Component\HttpKernel\Exception\MethodNotAllowedHttpException;
+use Symfony\Component\HttpKernel\Exception\NotFoundHttpException;
 
 class Handler extends ExceptionHandler
 {
@@ -61,8 +67,52 @@ class Handler extends ExceptionHandler
             return $this->errorResponse("Does not exists any ". $modelName ." with the specified identificator", 404);
         }
 
+        if ($exception instanceof AuthenticationException)
+        {
+            return $this->unauthenticated($request, $exception);
+        }
 
-        return parent::render($request, $exception);
+        if ($exception instanceof AuthorizationException)
+        {
+            return $this->errorResponse($exception->getMessage(), 403);
+        }
+
+        if ($exception instanceof NotFoundHttpException)
+        {
+            return $this->errorResponse('This URL not found', 404);
+        }
+
+        if ($exception instanceof MethodNotAllowedHttpException)
+        {
+            return $this->errorResponse('This method for the request is not valid', 405);
+        }
+
+        if ($exception instanceof HttpException)
+        {
+            return $this->errorResponse($exception->getMessage(), $exception->getStatusCode());
+        }
+
+        if ($exception instanceof QueryException)
+        {
+            $errorCode = $exception->errorInfo[1];
+            if ($errorCode == 1451)
+            {
+                return $this->errorResponse('Cannot remove this resource permanently. It is related with any other resource', 409);
+            }
+        }
+
+        if (config('app.debug'))
+        {
+            return parent::render($request, $exception);
+        }
+
+        return $this->errorResponse('Unexpected Exception. Try later', 500);
+
+    }
+
+    protected function unauthenticated($request, AuthenticationException $exception)
+    {
+        return $this->errorResponse('Unauthenticated', 401);
     }
 
     /**

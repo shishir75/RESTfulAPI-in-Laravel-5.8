8b65dfb75fac7ccaa121290e560a782658fab6c3
shishir75 <shishir.srdr16@gmail.com>
2020-01-14 22:27:30 +0000

    ProductCategoryController Added

---
+++ app/Http/Controllers/Product/ProductCategoryController.php
@@ -0,0 +1,22 @@
+<?php
+
+namespace App\Http\Controllers\Product;
+
+use App\Http\Controllers\ApiController;
+use App\Product;
+use Illuminate\Http\Request;
+
+class ProductCategoryController extends ApiController
+{
+    /**
+     * Display a listing of the resource.
+     *
+     * @return \Illuminate\Http\Response
+     */
+    public function index(Product $product)
+    {
+        $categories = $product->categories;
+
+        return $this->showAll($categories);
+    }
+}

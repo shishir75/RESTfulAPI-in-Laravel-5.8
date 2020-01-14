72403709d059c328419d3cffc75ebda3a8c9d086
shishir75 <shishir.srdr16@gmail.com>
2020-01-14 17:32:53 +0000

    SellerBuyerController Added

--- routes/api.php
+++ routes/api.php
@@ -28,6 +28,7 @@ Route::resource('products', 'Product\ProductController', ['only' => ['index', 's
 Route::resource('sellers', 'Seller\SellerController', ['only' => ['index', 'show']]);
 Route::resource('sellers.transactions', 'Seller\SellerTransactionController', ['only' => ['index']]);
 Route::resource('sellers.categories', 'Seller\SellerCategoryController', ['only' => ['index']]);
+Route::resource('sellers.buyers', 'Seller\SellerBuyerController', ['only' => ['index']]);
 Route::resource('transactions', 'Transaction\TransactionController', ['only' => ['index', 'show']]);
 Route::resource('transactions.categories', 'Transaction\TransactionCategoryController', ['only' => ['index']]);
 Route::resource('transactions.seller', 'Transaction\TransactionSellerController', ['only' => ['index']]);

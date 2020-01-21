af523ae4a7621720ec40cafa9778392da749cb39
shishir75 <shishir.srdr16@gmail.com>
2020-01-21 17:19:12 +0000

    Protecting all the Routes

--- app/Http/Controllers/User/UserController.php
+++ app/Http/Controllers/User/UserController.php
@@ -17,7 +17,7 @@ class UserController extends ApiController
     {
         $this->middleware('client.credentials')->only(['store','resend']);
 
-        $this->middleware('auth:api')->only(['store','resend', 'verify']);
+        $this->middleware('auth:api')->except(['store','resend', 'verify']);
 
         $this->middleware('transform.input:'. UserTransformer::class)->only(['store', 'update']);
     }

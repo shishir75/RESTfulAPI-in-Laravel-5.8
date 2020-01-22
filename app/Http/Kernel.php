0af182905b8668034237b9d82f1496c863b150b1
shishir75 <shishir.srdr16@gmail.com>
2020-01-22 19:04:50 +0000

    Registering the Laravel Passport Middleware to Check Scopes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -6,6 +6,8 @@ use App\Http\Middleware\SignatureMiddleware;
 use App\Http\Middleware\TransformInput;
 use Illuminate\Foundation\Http\Kernel as HttpKernel;
 use Laravel\Passport\Http\Middleware\CheckClientCredentials;
+use Laravel\Passport\Http\Middleware\CheckForAnyScope;
+use Laravel\Passport\Http\Middleware\CheckScopes;
 
 class Kernel extends HttpKernel
 {
@@ -68,6 +70,8 @@ class Kernel extends HttpKernel
         'signature' => SignatureMiddleware::class,
         'transform.input' => TransformInput::class,
         'client.credentials' => CheckClientCredentials::class,
+        'scope' => CheckForAnyScope::class,
+        'scopes' => CheckScopes::class,
     ];
 
     /**
